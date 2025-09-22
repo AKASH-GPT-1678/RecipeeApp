0b798e8d95e503c5fd925c2d42022c1aa553e3bc
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-09-23 00:13:15 +0530

    updated package.son

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,13 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-import { testDealer,createDealer , getDealers } from "../controllers/dealer.controller.js";
-
-
-
-router.post("/testDealer", testDealer);
-router.post("/dealer", createDealer);
-router.get("/getdealer", getDealers);
 
 
 
