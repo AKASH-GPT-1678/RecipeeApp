44c54dee23f312d8e43dc3daa224bd01a7001bdb
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-09-23 20:05:05 +0530

    expo app

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,21 @@
+import { Slot, Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { tokenCache } from "@clerk/clerk-expo/token-cache"
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+
+      <Slot />
+
+
+
+
+
+
+    </ClerkProvider>
+
+
+
+  )
+}
